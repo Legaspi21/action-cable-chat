c63709788df26c15c2ede5ac4eecd525dee810fb
Aidan Legaspi <aidanlegaspi1@gmail.com>
2017-01-28 14:43:19 -0800

    Set up routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+	 get '/signup', to: "registrations#new"
+  post '/signup', to: "registrations#create"
+
+  get 'login', to: "sessions#new"
+  post '/login', to: "sessions#create" 
+  delete '/logout', to: "sessions#destroy"
+
+  resources :chatrooms, param: :slug
+  resources :messages
 end
