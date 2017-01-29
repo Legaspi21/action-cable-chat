5eb5abea893d10e03f58a9a735480f69ad218904
Aidan Legaspi <aidanlegaspi1@gmail.com>
2017-01-28 17:08:38 -0800

    Change to singularization

--- db/migrate/20170128214759_create_messages.rb
+++ db/migrate/20170128214759_create_messages.rb
@@ -2,8 +2,8 @@ class CreateMessages < ActiveRecord::Migration[5.0]
   def change
     create_table :messages do |t|
       t.text :content
-      t.references :User, foreign_key: true
-      t.references :Chatroom, foreign_key: true
+      t.references :user, foreign_key: true
+      t.references :chatroom, foreign_key: true
 
       t.timestamps
     end
